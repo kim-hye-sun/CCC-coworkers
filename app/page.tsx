91b04821eefd1051ea1a1c94a26c3e525911f78c
eprnf <eprnf21@gmail.com>
2024-07-24 01:15:50 +0900

    CCC-4 🔀 Merge branch 'foo' into bar

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="inline-block bg-gradient-to-r from-red-500 via-green-500 to-violet-500 bg-clip-text text-6xl text-transparent">
-        Helloㅇㄹㅎ
+        Helloㅇㄹㅎㄴㅇㄹ
       </h1>
     </div>
   );
